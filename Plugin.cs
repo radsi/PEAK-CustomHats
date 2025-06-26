8e047fb2ef684594b923cc093983d3186395e7bf
radsi <78619513+radsi@users.noreply.github.com>
2025-06-26 11:40:29 +0200

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,15 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using BepInEx;
 using HarmonyLib;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Assertions;
-using UnityEngine.Windows;
-using Zorro.Core;
 
 namespace MoreHats
 {
@@ -17,13 +10,8 @@ namespace MoreHats
     public class Plugin : BaseUnityPlugin
     {
         public static AssetBundle assetBundle;
-        public static List<(GameObject, HatType)> customHats;
+        public static List<GameObject> customHats;
         public static List<Texture2D> customHatsIcons;
-
-        public enum HatType
-        {
-            Hat, Mask
-        };
 
         private class Patcher
         {
@@ -49,6 +37,7 @@ namespace MoreHats
             {
                 CreateHatOption(__instance.GetComponent<Customization>(), "Hat_GhostMask", customHatsIcons[0]);
                 CreateHatOption(__instance.GetComponent<Customization>(), "Hat_CreeperHat", customHatsIcons[1]);
+                CreateHatOption(__instance.GetComponent<Customization>(), "Hat_Wat", customHatsIcons[2]);
             }
             
             [HarmonyPatch(typeof(CharacterCustomization), "Awake")]
@@ -59,12 +48,29 @@ namespace MoreHats
 
                 for(int i = 0; i < customHats.Count; i++)
                 {
-                    GameObject newHat = Instantiate(customHats[i].Item1);
+                    GameObject newHat = Instantiate(customHats[i]);
                     newHat.GetComponent<MeshRenderer>().material.shader = Shader.Find("W/Character");
                     newHat.transform.SetParent(hatsObject);
-                    newHat.transform.localPosition = (customHats[i].Item2 == HatType.Mask ? new Vector3(0f, -1f, 5.6f) : new Vector3(0f, 0.5f, 8f));
-                    newHat.transform.localScale = (customHats[i].Item2 == HatType.Mask ? new Vector3(50f, 50f, 50f) : new Vector3(3f, 3f, 3f));
-                    newHat.transform.localRotation = (customHats[i].Item2 == HatType.Mask ? Quaternion.Euler(0f, 0f, 90f) : Quaternion.Euler(0f, 0f, 0f));
+
+                    if (newHat.name.Contains("creeper"))
+                    {
+                        newHat.transform.localPosition = new Vector3(0f, 0.5f, 8f);
+                        newHat.transform.localScale = new Vector3(3f, 3f, 3f);
+                        newHat.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                    }
+                    else if (newHat.name.Contains("ghost"))
+                    {
+                        newHat.transform.localPosition = new Vector3(0f, -1f, 5.6f);
+                        newHat.transform.localScale = new Vector3(50f, 50f, 50f);
+                        newHat.transform.localRotation = Quaternion.Euler(0f, 0f, 90f);
+                    }
+                    else
+                    {
+                        newHat.transform.localPosition = new Vector3(0f, 0f, 6.5f);
+                        newHat.transform.localScale = new Vector3(3f, 3f, 3f);
+                        newHat.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                    }
+
                     newHat.SetActive(false);
 
                     __instance.refs.playerHats = __instance.refs.playerHats.AddToArray(newHat.GetComponent<MeshRenderer>());
@@ -78,13 +84,15 @@ namespace MoreHats
 
             assetBundle = AssetBundle.LoadFromMemory(Resource1.hats);
 
-            customHats = new List<(GameObject, HatType)>();
+            customHats = new List<GameObject>();
             customHatsIcons = new List<Texture2D>();
 
-            customHats.Add((assetBundle.LoadAsset<GameObject>("assets/ghost.prefab"), HatType.Mask));
-            customHats.Add((assetBundle.LoadAsset<GameObject>("assets/creeper.prefab"), HatType.Hat));
+            customHats.Add((assetBundle.LoadAsset<GameObject>("assets/ghost.prefab")));
+            customHats.Add((assetBundle.LoadAsset<GameObject>("assets/creeper.prefab")));
+            customHats.Add((assetBundle.LoadAsset<GameObject>("assets/wat.prefab")));
             customHatsIcons.Add(assetBundle.LoadAsset<Texture2D>("assets/ghosticon.png"));
             customHatsIcons.Add(assetBundle.LoadAsset<Texture2D>("assets/creepericon.png"));
+            customHatsIcons.Add(assetBundle.LoadAsset<Texture2D>("assets/nagawat.png"));
         }
     }
 }
